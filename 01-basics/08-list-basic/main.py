19d946ac4168a7df21164f8c97df852751c2793f
devharu-49 <devharu2021@gmail.com>
2026-01-16 23:28:25 +0900

    Add list basic examples (Q01-Q03)

---
+++ 01-basics/08-list-basic/main.py
@@ -0,0 +1,16 @@
+# Q01: List Basic
+# リストを作成して表示する
+numbers = [1, 2, 3] #リストを作成する
+print(numbers) #リストを表示する
+
+# Q02: List Access
+# リストの要素にアクセスして表示する
+numbers = [1, 2, 3] #リストを作成する
+print(numbers[1]) #インデックス1の要素（2）を表示する
+
+# Q03: List Operations
+# リストの要素を変更して表示する
+numbers = [1, 2, 3] #リストを作成する
+numbers[2] = 4 #インデックス2の要素を4に変更する
+print(numbers) #変更後のリストを表示する
+
