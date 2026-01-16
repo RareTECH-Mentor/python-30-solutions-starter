b788be1b88dcd6317410c8f758bc186095a6d475
devharu-49 <devharu2021@gmail.com>
2026-01-16 22:32:46 +0900

    Add README and implementation for 02-variables

---
+++ 01-basics/02-variables/main.py
@@ -0,0 +1,13 @@
+# Q01: Variables Basic
+#20を変数ageに代入して
+age = 20
+My age is {age}
+
+# Q02: String Variable
+#Aliceを変数nameに代入して
+name = "Alice"
+My name is {name}
+
+# Q03: Multiple Variables
+#nameとageを変数に代入して
+My name is {name} and I am {age} years old
