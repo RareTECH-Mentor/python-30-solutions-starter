5a193654b9dbe1cc3e861f942f21b6ed7db6821c
devharu-49 <devharu2021@gmail.com>
2026-01-16 22:55:45 +0900

    Add if basic examples (Q01-Q03)

---
+++ 01-basics/04-if-basic/main.py
@@ -0,0 +1,24 @@
+# Q01: If Basic
+# ageが18以上のときに"Adult"を表示する
+age = 20 #20を変数ageに代入する
+if age >= 18: #ageが18以上のときに"Adult"を表示する
+    print("Adult")#"Adult"を表示する
+
+# Q02: If-Else
+# ageが18以上のときに"Adult"、それ以外のときに"Minor"を表示する
+age = 20 #20を変数ageに代入する
+if age >= 18: #ageが18以上のときに"Adult"を表示する
+    print("Adult")#"Adult"を表示する
+else: #ageが18未満のときに"Minor"を表示する
+    print("Minor")#"Minor"を表示する
+
+# Q03: If-Elif-Else
+# ageに応じて"Child"、"Teen"、"Adult"を表示する
+age = 15 #15を変数ageに代入する
+if age < 13: #ageが13未満のときに"Child"を表示する
+    print("Child")#"Child"を表示する
+elif age < 18: #ageが18未満のときに"Teen"を表示する
+    print("Teen")#"Teen"を表示する
+else: #ageが18以上のときに"Adult"を表示する
+    print("Adult")#"Adult"を表示する
+
