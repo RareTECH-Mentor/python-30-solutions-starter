f0879589690271d7fc5adb5169117d4e31078738
devharu-49 <devharu2021@gmail.com>
2026-01-23 23:02:33 +0900

    02-variables: add variables basic exercises

--- 01-basics/02-variables/main.py
+++ 01-basics/02-variables/main.py
@@ -1,13 +1,15 @@
 # Q01: Variables Basic
 #20を変数ageに代入して
 age = 20
-My age is {age}
+print(f"My age is {age}")
 
 # Q02: String Variable
 #Aliceを変数nameに代入して
 name = "Alice"
-My name is {name}
+print(f"My name is {name}")
 
 # Q03: Multiple Variables
 #nameとageを変数に代入して
-My name is {name} and I am {age} years old
+name = "Alice"
+age = 20
+print(f"My name is {name} and I am {age} years old")
